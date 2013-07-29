3ef778cc9fbdfefd90c9fa169a451e76feafedbb
Stuart Knightley <stuart@stuartk.com>
2013-07-29 12:16:51 -0700

    Move test code into separate module

--- integration.js
+++ integration.js
@@ -9,9 +9,7 @@ require('colors');
 var exec = require("./lib/exec");
 var install = require("./lib/install");
 var fixturesFor = require("./lib/fixtures-for");
-var serve = require("./lib/serve");
-var phantom = require("./lib/phantom");
-var run = require("./lib/run-page");
+var test = require("./lib/test");
 
 // FIXME: Q 0.9.6 uses process.nextTick and exceeds the maxTickDepth when
 // mopping Montage. As a temporary fix increase the limit.
@@ -91,37 +89,3 @@ install("mop", MOP_VERSION)
 })
 .done();
 
-function test(optimize, name, fs) {
-    console.log("Running Mop on " + name);
-
-    var config = {
-        fs: fs,
-        // If debug pass undefined so we get default output, otherwise disable
-        out: global.DEBUG ? void 0 : {}
-    };
-
-    return optimize("/", config)
-    .fail(function (error) {
-        return fs.listTree()
-        .then(function (tree) {
-            console.error(tree);
-            throw error;
-        });
-    })
-    .then(function (buildPath) {
-        var value = serve(fs, buildPath),
-            server = value[0],
-            url = value[1];
-        return phantom().then(function (browser) {
-            return run(browser, url + "index.html")
-            .finally(function () {
-                server.stop();
-                var quit = browser.quit();
-                // want to show any error messages from shutting down phantom
-                // and also wait until it is done
-                quit.done();
-                return quit;
-            });
-        });
-    });
-}
