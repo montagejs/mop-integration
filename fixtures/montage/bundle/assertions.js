0ac02114bd580502b0b63e6726ee4b04c14fb88f
François Frisch <francoisfrisch@gmail.com>
2014-03-14 19:01:01 -0700

    Add bundling test for Montage

---
+++ fixtures/montage/bundle/assertions.js
@@ -0,0 +1,28 @@
+var Q = require("q");
+var path = require("path");
+
+exports.shouldTestInBrowser = false;
+
+var APP_BUNDLE_PATH = "index.html.bundle-0.js";
+
+exports.run = function (fs, buildPath) {
+    return Q.all([
+        fs.stat(path.join(buildPath))
+        .then(function (stat) {
+            if (!stat.isDirectory()) {
+                return "Build directory not created";
+            }
+        }),
+        fs.stat(path.join(buildPath, APP_BUNDLE_PATH))
+        .then(function (stat) {
+            if (stat.isFile()) {
+                return fs.read(path.join(buildPath, APP_BUNDLE_PATH))
+            }
+        })
+        .then(function (content) {
+            if(content.indexOf("test_html_content") === -1) {
+                 return "HTML File not bundled";
+            }
+        })
+    ]);
+}
